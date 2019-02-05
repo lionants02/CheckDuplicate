9fdf8708875cf5fa390170853b302a9c81da9694
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-02-05 16:05:18 +0700

    fix bug. - server cloase

--- src/main/kotlin/max/kotlin/checkdupp/CheckDupplicateWithRest.kt
+++ src/main/kotlin/max/kotlin/checkdupp/CheckDupplicateWithRest.kt
@@ -4,12 +4,17 @@ import max.kotlin.checkdupp.webservice.WebServiceCheckerProcess
 
 class CheckDupplicateWithRest(private val processName: String) : CheckDupplicate {
     private val checkProcess: WebServiceCheckerProcess = WebServiceCheckerProcess(processName)
+    private lateinit var thread: Thread
 
     override fun register() {
         if (checkProcess.isDupplicate())
             throw DupplicateProcessException("rest duppliate $processName")
 
         checkProcess.lockProcess()
+        thread = Thread {
+            joinForLabTest()
+        }
+        thread.start()
     }
 
     internal fun joinForLabTest() {
